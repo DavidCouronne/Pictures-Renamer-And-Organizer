743fa1aad263a786c16cb2dc9d5521763a892fa7
COURONNE David <couronne.david@hotmail.fr>
2016-03-31 21:27:27 +0200

    Créateur

---
+++ Pictures-Renamer-And-Organizer/Pictures-Renamer-And-Organizer/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace Pictures_Renamer_And_Organizer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.openPictureButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // openPictureButton
+            // 
+            this.openPictureButton.Location = new System.Drawing.Point(12, 33);
+            this.openPictureButton.Name = "openPictureButton";
+            this.openPictureButton.Size = new System.Drawing.Size(107, 23);
+            this.openPictureButton.TabIndex = 0;
+            this.openPictureButton.Text = "Open Picture";
+            this.openPictureButton.UseVisualStyleBackColor = true;
+            this.openPictureButton.Click += new System.EventHandler(this.openPictureButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.openPictureButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button openPictureButton;
+    }
+}
+
---
+++ Pictures-Renamer-And-Organizer/Pictures-Renamer-And-Organizer/PictureViewer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pictures_Renamer_And_Organizer
+{
+    public partial class PicturesViewer : Form
+    {
+        public PicturesViewer()
+        {
+            InitializeComponent();
+        }
+    }
+}
