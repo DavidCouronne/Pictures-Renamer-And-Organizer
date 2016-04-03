7db9ddeeee58f2b0caf521b0b362a17397d3db54
COURONNE David <couronne.david@hotmail.fr>
2016-04-03 19:34:31 +0200

    Mise en forme

--- Pictures-Renamer-And-Organizer/Pictures-Renamer-And-Organizer/ExifData.cs
+++ Pictures-Renamer-And-Organizer/Pictures-Renamer-And-Organizer/ExifData.cs
@@ -25,28 +25,30 @@ namespace Pictures_Renamer_And_Organizer
                 str = str.Substring(0, nullCharIndex);
             return str;
         }
-        
-        public string ValueItem (int item)
+
+        public string ValueItem(int item)
         {
             return Convertisseur(item);
         }
         // Date Time
-        public string DateTake
+        public string DateTake // Retourne la date en format string
         {
-            get {
-                
-                return Convertisseur(FindDate()); }
+            get
+            {
+
+                return Convertisseur(FindDate());
+            }
         }
-        public string Maker
+        public string Maker //Retourne le fabriquand (normalement...)
         {
             get { return Convertisseur(0); }
         }
-        public string Model
+        public string Model //Retourne le modèle de l'appareil (normalement..)
         {
             get { return Convertisseur(1); }
         }
 
-        private DateTime ConvertisseurDate (string date )
+        private DateTime ConvertisseurDate(string date) //Retourne la date au format DateTime pour manipulations
         {
             //Si le format de date n'est pas bon, on retourne la date minimale, à tester dans le programme principal
             if (date.Length < 10)
@@ -59,28 +61,29 @@ namespace Pictures_Renamer_And_Organizer
             }
 
             return DateTime.ParseExact(date, "yyyy:MM:dd HH:mm:ss", CultureInfo.InvariantCulture);
-            
+
         }
 
         public DateTime DateFormatDate
         {
             get { return ConvertisseurDate(Convertisseur(FindDate())); }
         }
-//La date n'est pas toujours au meme endroit !!!
+        //La date n'est pas toujours au meme endroit !!!
         private int FindDate()
         {
             int counter = 0;
             int counter2 = 0;
             foreach (PropertyItem item in propItems)
             {
-                if (item.Value.Length == 20 )
-                {if (counter2 != 0) { return counter; }
+                if (item.Value.Length == 20)
+                {
+                    if (counter2 != 0) { return counter; }
                     counter2 = counter2 + 1;
                 }
                 counter = counter + 1;
 
             }
-            return 0;
+            return 0; // Retourne 0 si aucune date valide trouvée
         }
 
         public int ItemDate { get { return FindDate(); } }
--- Pictures-Renamer-And-Organizer/Pictures-Renamer-And-Organizer/PictureViewer.cs
+++ Pictures-Renamer-And-Organizer/Pictures-Renamer-And-Organizer/PictureViewer.cs
@@ -50,16 +50,16 @@ namespace Pictures_Renamer_And_Organizer
         private void ExifViewer(string Filename)
         {
             Image pict = Image.FromFile(Filename);
-            // Get the PropertyItems property from image.
+            // Récupère les propriétés de l'image
             PropertyItem[] propItems = pict.PropertyItems;
             // Initilisation 
             ExifData data = new ExifData();
             data.propItems = propItems;
-            richTextBox1.Text = richTextBox1.Text +  "\n" + "Date de prise: "+ data.DateTake + "   "+data.ItemDate;
+            richTextBox1.Text = richTextBox1.Text + "\n" + "Date de prise: " + data.DateTake + "   " + data.ItemDate;
             richTextBox1.Text = richTextBox1.Text + "\n" + "Marque de l'appareil: " + data.Maker;
             richTextBox1.Text = richTextBox1.Text + "\n" + "Modèle de l'appareil: " + data.Model;
             richTextBox1.Text = richTextBox1.Text + "\n" + "Année: " + data.DateFormatDate.Year.ToString();
-            //richTextBox1.Text = richTextBox1.Text + "\n" + "Mois: " + data.DateFormatDate.Month.ToString();
+            richTextBox1.Text = richTextBox1.Text + "\n" + "Mois: " + data.DateFormatDate.Month.ToString();
         }
 
         private void closeButton_Click(object sender, EventArgs e)
@@ -75,7 +75,7 @@ namespace Pictures_Renamer_And_Organizer
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
                 setImage(openFileDialog1.FileName);
-                
+
 
             }
         }
